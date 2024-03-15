7d4c709e5e2f9b3df8ce94a65c10c7c0fc18c6bf
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-03-15 18:21:23 +0100

    Sketch dummy app.

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from "./home/home.component";
+import { AdminComponent } from "./admin/admin.component";
+
+export const routes: Routes = [
+  { path: "admin", component: AdminComponent},
+  { path: "", component: HomeComponent},
+];
